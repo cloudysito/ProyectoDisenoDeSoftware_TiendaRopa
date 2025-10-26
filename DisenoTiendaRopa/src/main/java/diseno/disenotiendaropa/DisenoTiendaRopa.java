5a7129eb33d79f64714e1b6ddf42e590804ef30a
emiliano <emiimarqz@gmail.com>
2025-10-26 15:58:35 -0700

    Estructura Principal

---
+++ DisenoTiendaRopa/src/main/java/diseno/disenotiendaropa/DisenoTiendaRopa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package diseno.disenotiendaropa;
+
+/**
+ *
+ * @author emiim
+ */
+public class DisenoTiendaRopa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
