8fc2a526ce118aa8c508722bb9061249535499da
sansanty03 <138645645+sansanty03@users.noreply.github.com>
2025-11-27 04:41:20 -0700

    Revert "as"
    This reverts commit db6fa266b213e4a23b3ea9b09a13a44e57ef205c, reversing
    changes made to 6467518cb8beab04ee6fa4464cc6e22e61c3ee94.

---
+++ ObjetosNegocio/src/main/java/BOs/TallaBO.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BOs;
+
+import Exceptions.BOException;
+import Exceptions.DAOException;
+import Implementaciones.TallaDAO;
+import Interfaces.ITallaDAO;
+import com.mycompany.dto_negocio.TallaDTO;
+import mappers.TallaMapper;
+import objetosnegocio.dominioPojo.Talla;
+/**
+ *
+ * @author riosr
+ */
+public class TallaBO {
+    
+    private static TallaBO instance; 
+    
+    private ITallaDAO tallaDAO;
+
+    public TallaBO() {
+        this.tallaDAO = new TallaDAO();
+    }
+    
+    public static TallaBO getInstance(){
+        if(instance == null){
+            instance = new TallaBO();
+        }
+        return instance;
+    }
+    
+    //BO guardar talla
+    public TallaDTO guardarTalla(TallaDTO tallaDTO) throws BOException{
+        try{
+            Talla talla = TallaMapper.toEntity(tallaDTO);
+            
+            Talla tallaGuardar = tallaDAO.guardarTalla(talla);
+            
+            return TallaMapper.toDTO(tallaGuardar);
+            
+        }catch(DAOException e){
+            throw new BOException("Error al guardar empleado", e);
+        }
+    }
+    
+    //BO modificar talla
+    public TallaDTO modificarTalla(TallaDTO tallaDTO) throws BOException{
+        try{
+            Talla talla = TallaMapper.toEntity(tallaDTO);
+            
+            Talla tallaModificar = tallaDAO.modificarTalla(talla);
+            
+            return TallaMapper.toDTO(tallaModificar);
+            
+        }catch(DAOException e){
+            throw new BOException("Error al guardar empleado", e);
+        }
+    }
+    
+    //BO eliminar talla
+    public TallaDTO eliminarTalla(TallaDTO tallaDTO) throws BOException{
+        try{
+            Talla talla = TallaMapper.toEntity(tallaDTO);
+            
+            Talla tallaEliminar = tallaDAO.eliminarTalla(talla);
+            
+            return TallaMapper.toDTO(tallaEliminar);
+            
+        }catch(DAOException e){
+            throw new BOException("Error al guardar empleado", e);
+        }
+    }
+    
+    //BO buscar por id talla
+    public String buscarPorId(String idTalla) throws BOException{
+        try{
+            
+            return TallaDAO.buscarPorId(idTalla);
+            
+        }catch(DAOException e){
+            throw new BOException("Error al buscar por id empleado", e);
+        }
+    }
+    
+}
