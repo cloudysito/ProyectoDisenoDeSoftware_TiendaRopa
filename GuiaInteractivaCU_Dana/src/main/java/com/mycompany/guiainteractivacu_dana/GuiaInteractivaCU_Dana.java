f891cd4f272b83c23c46da22249db9a3fed97ace
Dana Rios <117124094+LU-12-12@users.noreply.github.com>
2025-11-27 22:49:48 -0700

    CU individual
    Creación de caso de uso individual Guia Interactiva

---
+++ GuiaInteractivaCU_Dana/src/main/java/com/mycompany/guiainteractivacu_dana/GuiaInteractivaCU_Dana.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guiainteractivacu_dana;
+
+/**
+ *
+ * @author riosr
+ */
+public class GuiaInteractivaCU_Dana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
