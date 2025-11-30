a712fa3c12541ef619bc9ab194cd191da5b5e51e
Dana Rios <117124094+LU-12-12@users.noreply.github.com>
2025-11-30 02:45:23 -0700

    Creacion de las BO
    Creacion de las BO de mi CU, metodos y datos de cada entidad dentro de mis CU estan sugetos a cambios. btw este ultimo comit tiene errores que le voy a preguntar al santigo mañana pq ya se durmio. Tengo sueño

---
+++ ObjetosNegocio/src/main/java/BOs/CapacitacionBO.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BOs;
+
+import BOs.Exception.BOException;
+import Exceptions.DAOException;
+import Implementaciones.CapacitacionDAO;
+import Interfaces.ICapacitacionDAO;
+import com.mycompany.dto_negocio.CapacitacionDTO;
+import mappers.CapacitacionMapper;
+
+/**
+ *
+ * @author riosr
+ */
+public class CapacitacionBO {
+    private static CapacitacionBO instance;
+
+    private ICapacitacionDAO capacitacionDAO;
+
+    public CapacitacionBO() {
+        this.capacitacionDAO = new CapacitacionDAO() {};
+    }
+
+    public static CapacitacionBO getInstance() {
+        if (instance == null) {
+            instance = new CapacitacionBO();
+        }
+        return instance;
+    }
+    
+    public CapacitacionDTO buscarPorId(String id) throws BOException {
+        try {
+
+            return CapacitacionMapper.toDTO(CapacitacionDAO.buscarPorId(id));
+
+        } catch (DAOException e) {
+            throw new BOException("Error al buscar por id empleado", e);
+        }
+    }
+}
---
+++ ObjetosNegocio/src/main/java/BOs/RecompenzaBO.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BOs;
+
+import BOs.Exception.BOException;
+import Exceptions.DAOException;
+import Implementaciones.RecompenzaDAO;
+import Interfaces.IRecompenzaDAO;
+import com.mycompany.dto_negocio.RecompenzaDTO;
+import mappers.RecompenzaMapper;
+
+/**
+ *
+ * @author riosr
+ */
+public class RecompenzaBO {
+    private static RecompenzaBO instance;
+
+    private IRecompenzaDAO recompenzaDAO;
+
+    public RecompenzaBO() {
+        this.recompenzaDAO = new RecompenzaDAO() {};
+    }
+
+    public static RecompenzaBO getInstance() {
+        if (instance == null) {
+            instance = new RecompenzaBO();
+        }
+        return instance;
+    }
+    
+    public RecompenzaDTO buscarPorId(String id) throws BOException {
+        try {
+
+            return RecompenzaMapper.toDTO(RecompenzaDAO.buscarPorId(id));
+
+        } catch (DAOException e) {
+            throw new BOException("Error al buscar por id empleado", e);
+        }
+    }
+}
