498b6cfef5b6e3ef411c30d6d6ccd4a1f5a3861f
Majo8805 <maria.enriquez252337@potros.itson.edu.mx>
2025-12-01 23:04:11 -0700

    Creacion de subsistema GestionarSugerencias

---
+++ GestionarSugerenciasSubsystem/src/main/java/Interfaz/IGestionarSugerencias.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaz;
+
+import com.mycompany.dto_negocio.SugerenciaDTO;
+import java.util.List;
+
+/**
+ *
+ * @author garfi
+ */
+public interface IGestionarSugerencias {
+
+    public List<SugerenciaDTO> filtrarSugerencias(String filtro);
+
+    public boolean cambiarEstadoSugerencia(SugerenciaDTO sugerencia, String nuevoEstado);
+}
